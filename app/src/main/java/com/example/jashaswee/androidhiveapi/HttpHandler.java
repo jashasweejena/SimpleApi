56acd06e1dc8137c2f96ced1f3a18ca84de1191b
Jashaswee Jena <j.ishujena2000@gmail.com>
2018-03-31 01:02:06 +0530

    Initial

---
+++ app/src/main/java/com/example/jashaswee/androidhiveapi/HttpHandler.java
@@ -0,0 +1,71 @@
+package com.example.jashaswee.androidhiveapi;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by jashaswee on 30/3/18.
+ */
+
+public class HttpHandler {
+
+    public static final String TAG = HttpHandler.class.getSimpleName();
+
+    public HttpHandler()
+
+    {
+
+    }
+
+    public String makeServiceCall(String reqUrl)
+    {
+        String response = null;
+
+        try {
+            URL url = new URL(reqUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            //Read the response
+
+            InputStream in = new BufferedInputStream(conn.getInputStream());
+            response = convertStreamToString(in);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+
+    private String convertStreamToString(InputStream is)
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try{
+            while((line = reader.readLine())!=null){
+                sb.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try{
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+
+}
